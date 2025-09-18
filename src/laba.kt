7b88242261a67735e6ac2cb129cc8a305f3581f8
Nadia Rykhlyuk <nadigar1506@mail.ru>
2025-09-18 07:57:31 +0300

    Задание 2

---
+++ src/laba.kt
@@ -0,0 +1,17 @@
+fun main() {
+    //Задание 1
+//    showGameLogo()
+//    showGameLogo()
+
+    //Задание 2
+//    showEnemy("Гоблин", 22)
+//    showEnemy("Орк", 45)
+}
+//Задание 1
+fun showGameLogo() {
+    println("=== EPIC QUEST ===")
+}
+//Задание 2
+fun showEnemy(name: String, level: Int) {
+    println("Враг: ${name} (Уровень: ${level})")
+}
