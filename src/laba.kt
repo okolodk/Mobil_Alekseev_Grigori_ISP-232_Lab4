e27662daddedbd87ee57537b643eb9750f20c2c9
Nadia Rykhlyuk <nadigar1506@mail.ru>
2025-09-18 08:06:13 +0300

    Задание 3

--- src/laba.kt
+++ src/laba.kt
@@ -6,6 +6,11 @@ fun main() {
     //Задание 2
 //    showEnemy("Гоблин", 22)
 //    showEnemy("Орк", 45)
+
+    //Задание 3
+//    spawnEnemy("гоблин")
+//    spawnEnemy("Вождь гоблинов", 200,true)
+
 }
 //Задание 1
 fun showGameLogo() {
@@ -14,4 +19,9 @@ fun showGameLogo() {
 //Задание 2
 fun showEnemy(name: String, level: Int) {
     println("Враг: ${name} (Уровень: ${level})")
+}
+//Задание 3
+fun spawnEnemy(name: String, health: Int = 100, isBoss:
+Boolean = false) {
+    println("Враг: $name (Здоровье: $health HP) ${if (isBoss)  "БОСС!" else ""}")
 }
